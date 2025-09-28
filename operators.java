3b7b13c20cd803f290d4b93d0cb14b2bf6727a89
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-09-29 05:27:01 +0530

    Create operators.java
    3

---
+++ operators.java
@@ -0,0 +1,53 @@
+// Operations in JAVA
+/*
+arithmetic
+assignment
+logical
+comparison
+*/
+public class main{
+    public static void main(String[] args){
+        
+        //comparison
+        
+        int num1 = 45, num2 = 5;
+        num1 += 3;
+        num2 -= 3;
+        
+        System.out.print("The walue of num1 + num2 is:");
+        System.out.println(num1 + num2);
+        
+        System.out.print("The walue of num1 - num2 is:");
+        System.out.println(num1 - num2);
+        
+        System.out.print("The walue of num1 * num2 is:");
+        System.out.println(num1 * num2);
+        
+        System.out.print("The walue of num1 / num2 is:");
+        System.out.println(num1 / num2);
+        
+        System.out.print("The walue of num1 % num2 is:");
+        System.out.println(num1 % num2);
+        
+        //assingment
+        
+        System.out.println(num1++);
+        System.out.println(++num1);
+        System.out.println(num1--);
+        System.out.println(--num1);
+
+    }
+}
+
+        /*comparison
+        == -> equality of 2 values
+        != -> not equal
+        <
+        > 
+        <=
+        >=
+        
+        Logical 
+        &&
+        ||
+        !
