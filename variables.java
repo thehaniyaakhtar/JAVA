04d8f427404aba4c314daba140690c440a5286d1
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-09-28 20:02:25 +0530

    2

---
+++ variables.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "ABC";
+        System.out.println(name);
+        
+        int a = 34;
+        float b = 45.6f;
+        System.out.println(b);
+        
+        boolean isAdult = false;
+        System.out.println(a);
+        
+        System.out.println(isAdult);
+        
+        int x = 3, y = 4, z = 8;
+        System.out.println(z);
+    }
+}
+
+/* constructing name in JAVA:
+- 1 2 3, _, $, a b c
+- no whitespaces
+- case sensitive
+- no reserved keywords
+- begin with a b c, _, $
+*/
