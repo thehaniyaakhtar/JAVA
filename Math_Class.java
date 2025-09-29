4ec556c74068e411ee7d1447a7f43ce0ec213862
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-09-29 07:26:53 +0530

    6

---
+++ Math_Class.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        
+        int num = 4, num1 = 6;
+        System.out.println(Math.max(num, num1));
+        System.out.println(Math.min(num, num1));
+        System.out.println(Math.sqrt(36));
+        System.out.println(Math.abs(-36));
+        System.out.println(Math.abs(5));
+        System.out.println(Math.random()); // between 0 and 1
+        System.out.println(4+(8-4)*Math.random());  // between 4 and 8
+        
+    }
+}
