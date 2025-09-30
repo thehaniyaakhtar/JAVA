3900bd3db5c5c9ce0a696876950ac2b4ab5b8f63
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-09-30 23:11:40 +0530

    11

---
+++ basic_syntax.java
@@ -0,0 +1,20 @@
+class Main{ 
+    // class wherein every program resides
+    // What a program has and what it can do
+    // An object is created from a class blueprint
+    
+    public static void main(String[] args){
+        // method where the program starts
+        System.out.println("Hello");
+        // prints text on to screen
+        
+        int age = 22;
+        String name = "Haniya";
+        System.out.println(name.length());
+        System.out.println(name.charAt(2));
+        
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+        
+        
+    }
+}
